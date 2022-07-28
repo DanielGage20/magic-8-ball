1225c15d88161314d0be14558f21862a5c7a7b56
DanielGage20 <danielgage20@gmail.com>
2022-07-28 13:05:43 +0000

    trying to write javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,20 @@
-import random
 
-name = ''
-question = 'Are you ready?'
-answer = ''
-random_number = random.randint(1, 10)
-# print(random_number)
 
-if random_number == 1:
-  answer = 'Yes - definitely.'
-elif random_number == 2:
-  answer = 'It is decidedly so.'
-elif random_number == 3:
-  answer = 'Without a doubt.'
-elif random_number == 4:
-  answer = 'Reply hazy, try again.'
-elif random_number == 5:
-  answer = 'Ask again later.'
-elif random_number == 6:
-  answer = 'Better not tell you now.'
-elif random_number == 7:
-  answer = 'My sources say no.'
-elif random_number == 8:
-  answer = 'Outlook not so good.'
-elif random_number == 9:
-  answer = 'Very doubtful.'
-elif random_number == 10:
-  answer = 'Yeah deffo mate'
-else:
-  answer = 'Error'
+document.addEventListener("DOMContentLoaded", function() {
+  let buttons = document.getElementsById("questionButton");
+}
 
-if name =='':
-  print('Question: ' + question)
-else:
-    print(name + " asks: " + question)
-print('Magic 8-Balls answer: ' + answer)
 
-if question == '':
-  print('The magic 8-Ball cannot provide a fortune unless you ask it something.')
-else:
-    print(name + " asks: " + question)
-    print('Magic 8-Balls answer: ' + answer)
+  let answers = ['Most likely!',
+               'Good luck!',
+               '100% yes!',
+               'The future is yours!',
+               'I better not tell you now!',
+               'Highly doubtful!',
+               'Pick another route!!',
+               'Slim chance!',
+               'It is all upto you!!',
+               'The future is yours!',
+               'Not happening, sorrry!',
+               'Try another question!',
+]
