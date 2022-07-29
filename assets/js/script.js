9816a5d3e0dd23f601aed23de7b3ab1febc4de5d
DanielGage20 <danielgage20@gmail.com>
2022-07-29 15:53:06 +0000

    trying to link box and random answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,23 @@
-
-var onClick function() {
-
-}
-var question = prompt("What's your future?");
-8Ball.getAnswer(question);
-};
-$("#questionButton").click( onClick );
-}
-
-8ball.answers = function(questionbutton)
-{
-  var randomitem = answers[Math.floor(Math.randomitem()*answers.length)]
-}
-
-  var answers = [
-               "Most likely!",
-               "Good luck!",
-               "100% yes!",
-               "The future is yours!",
-               "I better not tell you now!",
-               "Highly doubtful!",
-               "Pick another route!!",
-               "Slim chance!",
-               "It is all upto you!",
-               "The future is yours!",
-               "Not happening, sorry!",
-               "Try another question!"
+ // Generates a random answer when user asks a question
+ 
+let answers = [
+             "Most likely!",
+             "Good luck!",
+             "100% yes!",
+             "The future is yours!",
+             "I better not tell you now!",
+             "Highly doubtful!",
+             "Pick another route!",
+             "Slim chance!",
+             "It is all upto you!",
+             "The future is yours!",
+             "Not happening, sorry!",
+             "Try another question!"
 ];
 
-console.log(magic8ball)
+function questionButton() {
+  var userAdjective = prompt("What's on your mind?");
+  return Math.random
+}
+
+
