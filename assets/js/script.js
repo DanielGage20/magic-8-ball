7413ec60828bdd16bdc019f46988c4a0c5ba0088
DanielGage20 <danielgage20@gmail.com>
2022-08-01 16:06:12 +0000

    finished JS project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,51 @@
- // Generates a random answer when user asks a question
+ //* Generates a random answer when user asks a question
 
- const answers = [];
-answers[0]= "Most likely!";
-answers[1]= "Good luck!";
-answers[2]= "100% yes!";
-answers[3]= "The future is yours!";
-answers[4]= "I better not tell you now!";
-answers[5]= "Highly doubtful!";
-answers[6]= "Pick another route!";
-answers[7]= "Slim chance!";
-answers[8]= "It's all upto you!";
-answers[9]= "The future is yours!";
-answers[10]= "Not happening, sorry!";
-answers[11]= "Try another question!";
-
-function questionButton() {
-  var userAdjective = prompt("What's on your mind?");
-  const random = Math.random() * answers.length;
+ function questionButton() {
+  prompt ("What do you want to know?");
+  window.alert(answer);
 };
 
-console.log(questionButton);
-console.log(random, answers[random]);
+ let randomNumber = Math.floor(Math.random() * 12);
+ let answer = '';
 
+ switch (randomNumber) {
+  case 0:
+    answer = 'Most likely!';
+    break;
+    case 1:
+      answer = 'Good Luck!';
+      break;
+      case 2:
+      answer = '100% yes';
+      break;
+      case 3:
+      answer = 'The future is yours!';
+      break;
+      case 4:
+      answer = 'I better not tell you now!';
+      break;
+      case 5:
+      answer = 'Highly doubtful!';
+      break;
+      case 6:
+      answer = 'Pick another route!';
+      break;
+      case 7:
+      answer = 'Slim chance!';
+      break;
+      case 8:
+      answer = 'It is all upto you!';
+      break;
+      case 9:
+      answer = 'The future is yours!';
+      break;
+      case 10:
+      answer = 'Not happening, sorry!';
+      break;
+      case 11:
+      answer = 'Try another question!';
+      break;
+ }
+ 
+console.log(answer);
 
